2fe9dda4d92bc879eb305a7aeeae6cce76bfc27e
Amlandeep Bhadra <amlandeep1912@gmail.com>
2022-12-08 14:02:54 -0500

    Add metrics server with customizable address

--- cmd/archive-access-api/main.go
+++ cmd/archive-access-api/main.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"errors"
+	"github.com/onflow/flow-archive-access/metrics"
 	"google.golang.org/grpc/credentials/insecure"
 	"net"
 	"net/http"
@@ -57,15 +58,17 @@ func run() int {
 
 	// Command line parameter initialization.
 	var (
-		flagAddress string
-		flagArchive string
-		flagCache   uint64
-		flagLevel   string
+		flagAddress     string
+		flagArchive     string
+		flagCache       uint64
+		flagMetricsAddr string
+		flagLevel       string
 	)
 
 	pflag.StringVarP(&flagAddress, "address", "a", "127.0.0.1:9000", "address to serve Access API on")
 	pflag.StringVarP(&flagArchive, "archive", "d", "127.0.0.1:80", "host URL for Archive API endpoint")
 	pflag.StringVarP(&flagLevel, "level", "l", "info", "log output level")
+	pflag.StringVarP(&flagMetricsAddr, "metrics", "m", "", "address on which to expose metrics (no metrics are exposed when left empty)")
 
 	pflag.Uint64Var(&flagCache, "cache-size", 1_000_000_000, "maximum cache size for register reads in bytes")
 
@@ -126,6 +129,7 @@ func run() int {
 		log.Error().Str("address", flagAddress).Err(err).Msg("could not listen")
 		return failure
 	}
+	metricsEnabled := flagMetricsAddr != ""
 	done := make(chan struct{})
 	failed := make(chan struct{})
 	go func() {
@@ -141,7 +145,19 @@ func run() int {
 		}
 		log.Info().Msg("Flow Access API Server stopped")
 	}()
+	go func() {
+		if !metricsEnabled {
+			return
+		}
 
+		log.Info().Msg("metrics server starting")
+		server := metrics.NewServer(log, flagMetricsAddr)
+		err := server.Start()
+		if err != nil {
+			log.Warn().Err(err).Msg("metrics server failed")
+		}
+		log.Info().Msg("metrics server stopped")
+	}()
 	select {
 	case <-sig:
 		log.Info().Msg("Flow Access API Server stopping")
---
+++ metrics/server.go
@@ -0,0 +1,40 @@
+package metrics
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/rs/zerolog"
+)
+
+// Server is the http server that will be serving the /metrics request for prometheus.
+type Server struct {
+	server *http.Server
+	log    zerolog.Logger
+}
+
+// NewServer creates a new server that exposes metrics.
+func NewServer(log zerolog.Logger, address string) *Server {
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+
+	m := Server{
+		server: &http.Server{
+			Addr:    address,
+			Handler: mux,
+		},
+		log: log,
+	}
+
+	return &m
+}
+
+// Start registers the metrics and launches the server.
+func (s *Server) Start() error {
+	err := s.server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("could not listen and serve: %w", err)
+	}
+	return nil
+}
